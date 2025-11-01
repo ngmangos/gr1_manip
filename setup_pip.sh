fb5917f341bc7788c4c5b50c04428f06708e154c
Fourier PC <blinky.unswathome@gmail.com>
2025-11-01 14:07:17 +0000

    adding on policy runner to setup

--- setup_pip.sh
+++ setup_pip.sh
@@ -1,2 +1,4 @@
 python3 -m pip install -e source/gr1_train
 pip3 install matplotlib
+cp -f /workspace/fourier-sim/gr1_train/algos/on_policy_runner.py \
+    /workspace/isaaclab/_isaac_sim/kit/python/lib/python3.11/site-packages/rsl_rl/runners/on_policy_runner.py
