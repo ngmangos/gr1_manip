acc8207ffea26e3ef3d0d86bcbace92151ddfd1d
Fourier PC <blinky.unswathome@gmail.com>
2025-11-01 14:25:33 +0000

    save copy orig policy runner

--- setup_pip.sh
+++ setup_pip.sh
@@ -1,4 +1,6 @@
 python3 -m pip install -e source/gr1_train
 pip3 install matplotlib
+mv -f /workspace/isaaclab/_isaac_sim/kit/python/lib/python3.11/site-packages/rsl_rl/runners/on_policy_runner.py \
+    /workspace/isaaclab/_isaac_sim/kit/python/lib/python3.11/site-packages/rsl_rl/runners/on_policy_runner_cpy.py
 cp -f /workspace/fourier-sim/gr1_train/algos/on_policy_runner.py \
     /workspace/isaaclab/_isaac_sim/kit/python/lib/python3.11/site-packages/rsl_rl/runners/on_policy_runner.py
