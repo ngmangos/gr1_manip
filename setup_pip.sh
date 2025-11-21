d86cbd5fd311dc927498d60113d4e96cc80f9f5b
Fourier PC <blinky.unswathome@gmail.com>
2025-11-21 14:56:01 +0000

    Adding graphs

--- setup_pip.sh
+++ setup_pip.sh
@@ -1,4 +1,4 @@
-python3 -m pip install -e source/gr1_train
+python3 -m pip install -e /workspace/fourier-sim/gr1_train/source/gr1_train
 pip3 install matplotlib
 mv -f /workspace/isaaclab/_isaac_sim/kit/python/lib/python3.11/site-packages/rsl_rl/runners/on_policy_runner.py \
     /workspace/isaaclab/_isaac_sim/kit/python/lib/python3.11/site-packages/rsl_rl/runners/on_policy_runner_cpy.py
